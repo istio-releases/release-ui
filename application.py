8d61008a7553b1f44da6d692195fd28ad266ee8c
cabreraem <emiliacabrera@emiliacabrera.svl.corp.google.com>
2018-06-04 16:27:29 -0700

    begin implementing javascript front end

--- application.py
+++ application.py
@@ -1,42 +1,8 @@
 from flask import Flask, render_template, request
-from google.appengine.ext import ndb
 import datetime
-import data
+
+
 app = Flask(__name__)
-
-# maybe we need it, probably we won't
-# class Modification(ndb.Model):
-#     change_url = ndb.StringProperty()
-#     timestamp = ndb.DateTimeProperty()
-#
-# class Release(ndb.Model):
-#     version = ndb.StringProperty()
-#     creation = ndb.DateTimeProperty(auto_now_add=True)
-#     last_change = ndb.DateTimeProperty(auto_now=True)
-#     changes = ndb.StringProperty(repeated=True)
-#     last_task = ndb.KeyProperty()
-#     status = ndb.BooleanProperty()
-#     tag = ndb.IntegerProperty()
-#     build_artifacts = ndb.StringProperty()
-#     branch = ndb.StringProperty()
-#     tasks = ndb.KeyProperty(repeated=True)
-#
-# class ReleaseTask(ndb.Model):
-#     task_name = ndb.StringProperty()
-#     start = ndb.DateTimeProperty()
-#     end = ndb.DateTimeProperty()
-#     status = ndb.BooleanProperty()
-#     error_message = ndb.StringProperty()
-#     log_link = ndb.StringProperty()
-#     release_key = ndb.KeyProperty()
-#
-# # adds the "Test_Post" entity kind to the datastore, with the following properties:
-# class Test_Post(ndb.Model):
-#     name = ndb.StringProperty()
-#     email = ndb.StringProperty()
-#     site = ndb.StringProperty()
-#     comments = ndb.StringProperty()
-
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -60,7 +26,7 @@ def details():
     fake ={"version" : "xxx-xxx-xxx", "status" : True, "creation" : "mm/dd/yy at hh:mm:ss", "last_mod" : "mm/dd/yy at hh:mm:ss", "last_active" : "task123", "tag" : 1, "github" : "https://github.com/cabreraem/mock_release_UI"}
     task = {"status": False, "name": "test123", "start" : "mm/dd/yy at hh:mm:ss", "duration" : "xxx units", "log" : "https://github.com/cabreraem/mock_release_UI"}
     fakeTasks = [task]
-    
+
     return render_template('details.html', release=fake, tasks=fakeTasks)
 
 # @app.route('/form')
--- static/scripts.js
+++ static/scripts.js
@@ -2,8 +2,34 @@ function handle(e){
 	search=document.getElementById("search-box").value;
     if(e.keyCode === 13){
       $.post("receiver", search, function(){
-
       });
     }
 	return false;
 }
+
+// Execute when DOM is fully loaded
+$(document).ready(function() {
+	var dataObject = {
+		0: {"version":"xxx-xxx-123","status":true,"creation":"mm/dd/yy at hh:mm:ss","last_mod":"mm/dd/yy at hh:mm:ss","last_active":"task123","tag":1},
+		1: {"version":"xxx-xxx-456","status":true,"creation":"mm/dd/yy at hh:mm:ss","last_mod":"mm/dd/yy at hh:mm:ss","last_active":"task456","tag":2}
+	};
+
+	// Cache of the template
+	var template = document.getElementById("template-dashboard-list");
+	// Get the contents of the template
+	var templateHtml = template.innerHTML;
+	// Final HTML variable as empty string
+	var listHtml = "";
+
+	// Loop through dataObject, replace placeholder tags
+	// with actual data, and generate final HTML
+	for (var key in dataObject) {
+	  listHtml += templateHtml.replace(/{{version}}/g, dataObject[key]["version"])
+	                          .replace(/creation/g, dataObject[key]["creation"])
+	                          .replace(/{{last_change}}/g, dataObject[key]["last_mod"])
+	                          .replace(/{{last_active}}/g, dataObject[key]["last_active"])
+	                          .replace(/{{tag}}/g, dataObject[key]["tag"]);
+	}
+
+	document.getElementById("dashboard-list").innerHTML = listHtml;
+});
