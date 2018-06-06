5df3c3d9965403120054495dfb528d74806cf602
cabreraem <emiliacabrera@emiliacabrera.svl.corp.google.com>
2018-06-05 17:10:12 -0700

    Merge branch 'master' of github.com:cabreraem/release-ui
    added sortTable function

--- static/scripts.js
+++ static/scripts.js
@@ -57,27 +57,24 @@ function tagFilter(label) {
 }
 
 // function source: https://www.w3schools.com/howto/howto_js_sort_table.asp
-function sortTable() {
+function sortTable(column) {
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("dashboard");
   switching = true;
 
-  /* Make a loop that will continue until
-  no switching has been done: */
+  /* loop that will continue until no switching has been done: */
   while (switching) {
 
-    // Start by saying: no switching is done:
     switching = false;
     rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
+
+    for (i = 0; i < rows.length; i++) {
       shouldSwitch = false;
       /* Get the two elements you want to compare,
       one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
+      x = rows[i].getElementsByTagName("td")[column];
+      y = rows[i + 1].getElementsByTagName("td")[column];
+
       // Check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
         // If so, mark as a switch and break the loop:
