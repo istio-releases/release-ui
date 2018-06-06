a43814c98395ab858a8567955cb8c5ac016fabaa
cabreraem <emiliacabrera@emiliacabrera.svl.corp.google.com>
2018-06-06 10:31:27 -0700

    finished table sorting, beginning date selector

--- application.py
+++ application.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def index():
     """Main index is the dashboard with a table of recent releases"""
 
-    fake1 = {"version":"xxx-xxx-123","status":1,"creation":"mm/dd/yy at hh:mm:ss","last_mod":"mm/dd/yy at hh:mm:ss","last_active":"task123","tags":[1,3]}
-    fake2 = {"version":"xxx-xxx-456","status":2,"creation":"mm/dd/yy at hh:mm:ss","last_mod":"mm/dd/yy at hh:mm:ss","last_active":"task456","tags":[2]}
-    fake3 = {"version":"xxx-xxx-789","status":3,"creation":"mm/dd/yy at hh:mm:ss","last_mod":"mm/dd/yy at hh:mm:ss","last_active":"task789","tags":[4]}
-    fake4 = {"version":"xxx-xxx-000","status":4,"creation":"mm/dd/yy at hh:mm:ss","last_mod":"mm/dd/yy at hh:mm:ss","last_active":"task000","tags":[1,2]}
+    fake1 = {"version":"xxx-xxx-123","status":1,"creation":"01 Jan 1970 00:00:00","last_mod":"01 Jan 1970 00:00:00","last_active":"task123","tags":[1,3]}
+    fake2 = {"version":"xxx-xxx-456","status":2,"creation":"04 Dec 2017 00:12:00","last_mod":"04 Dec 2017 00:12:00","last_active":"task456","tags":[2]}
+    fake3 = {"version":"xxx-xxx-789","status":3,"creation":"29 Jun 1999 4:30:30","last_mod":"29 Jun 1999 4:30:30","last_active":"task789","tags":[4]}
+    fake4 = {"version":"xxx-xxx-000","status":4,"creation":"04 Dec 1995 00:12:00","last_mod":"04 Dec 1995 00:12:00","last_active":"task000","tags":[1,2]}
     fakeData = [fake1, fake2, fake3, fake4]
 
     return render_template('index.html', releases= fakeData)
--- static/scripts.js
+++ static/scripts.js
@@ -4,14 +4,14 @@ function searchBar() {
   var input, filter, table, tr, td, i;
   input = document.getElementById("search-box");
   filter = input.value.toUpperCase();
-  table = document.getElementById("dashboard-list");
+  table = document.getElementById("dashboard");
   rows = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
 	for (i = 0; i < rows.length; i++) {
 		var exists = false
 		for (j = 0; j < rows[i].getElementsByTagName("td").length; j++){
-			td = tr[i].getElementsByTagName("td")[j];
+			td = rows[i].getElementsByTagName("td")[j];
 			if (td) {
 				if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
 					exists = true;
@@ -56,6 +56,7 @@ function tagFilter(label) {
 	filter(label, 5)
 }
 
+// Sort table alphabetically or reverse chronologically depending on column clicked
 // function source: https://www.w3schools.com/howto/howto_js_sort_table.asp
 function sortTable(column) {
   var table, rows, switching, i, x, y, shouldSwitch;
@@ -68,15 +69,22 @@ function sortTable(column) {
     switching = false;
     rows = table.getElementsByTagName("tr");
 
-    for (i = 0; i < rows.length; i++) {
+    for (i = 0; i < (rows.length - 1); i++) {
       shouldSwitch = false;
       /* Get the two elements you want to compare,
       one from current row and one from the next: */
       x = rows[i].getElementsByTagName("td")[column];
       y = rows[i + 1].getElementsByTagName("td")[column];
 
+      var compare;
+      if (column == 2 || column == 3){
+        compare = Date.parse(x.innerHTML) < Date.parse(y.innerHTML)
+      }
+      else {
+        compare = x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()
+      }
       // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+      if (compare) {
         // If so, mark as a switch and break the loop:
         shouldSwitch = true;
         break;
@@ -90,3 +98,8 @@ function sortTable(column) {
     }
   }
 }
+
+// Sort by Last Modified date when DOM is fully loaded
+$(document).ready(function() {
+    sortTable(3)
+});
