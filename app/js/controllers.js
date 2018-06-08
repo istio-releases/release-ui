64367bc87b432e2a2549209d610ba3723e201fcf
DomMarq <dommarques@google.com>
2018-06-08 16:30:47 -0700

    testing REST api/http calls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,13 @@
-var fake1 = {"version":"xxx-xxx-123","status":1,"creation":"01 Jan 1970 00:00:00","last_mod":"01 Jan 1970 00:00:00","last_active":"task123","tags":[1,3]}
-var fake2 = {"version":"xxx-xxx-456","status":2,"creation":"04 Dec 2017 00:12:00","last_mod":"04 Dec 2017 00:12:00","last_active":"task456","tags":[2]}
-var fake3 = {"version":"xxx-xxx-789","status":3,"creation":"29 Jun 1999 4:30:30","last_mod":"29 Jun 1999 4:30:30","last_active":"task789","tags":[4]}
-var fake4 = {"version":"xxx-xxx-000","status":4,"creation":"04 Dec 1995 00:12:00","last_mod":"04 Dec 1995 00:12:00","last_active":"task000","tags":[1,2]}
-var fakeData = [fake1, fake2, fake3, fake4]
+// var fake1 = {"version":"xxx-xxx-123","status":1,"creation":"01 Jan 1970 00:00:00","last_mod":"01 Jan 1970 00:00:00","last_active":"task123","tags":[1,3]}
+// var fake2 = {"version":"xxx-xxx-456","status":2,"creation":"04 Dec 2017 00:12:00","last_mod":"04 Dec 2017 00:12:00","last_active":"task456","tags":[2]}
+// var fake3 = {"version":"xxx-xxx-789","status":3,"creation":"29 Jun 1999 4:30:30","last_mod":"29 Jun 1999 4:30:30","last_active":"task789","tags":[4]}
+// var fake4 = {"version":"xxx-xxx-000","status":4,"creation":"04 Dec 1995 00:12:00","last_mod":"04 Dec 1995 00:12:00","last_active":"task000","tags":[1,2]}
+// var fakeData = [fake1, fake2, fake3, fake4]
 
-App.controller('MainController', function($scope) {
+
+
+
+App.controller('MainController', function($scope,$http) {
+  var fakeData = $http.get('/');
   $scope.releases = fakeData;
 });
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse
+import read_fake_data
+
 
 
 # creating the Flask application
 app = Flask(__name__)
-
-
-@app.route('/')
+api = Api(app)
+read_fake_data # read and load the data from fake_data.py into memcache
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    fake1 = {"version":"xxx-xxx-123","status":1,"creation":"01 Jan 1970 00:00:00","last_mod":"01 Jan 1970 00:00:00","last_active":"task123","tags":[1,3]}
-    fake2 = {"version":"xxx-xxx-456","status":2,"creation":"04 Dec 2017 00:12:00","last_mod":"04 Dec 2017 00:12:00","last_active":"task456","tags":[2]}
-    fake3 = {"version":"xxx-xxx-789","status":3,"creation":"29 Jun 1999 4:30:30","last_mod":"29 Jun 1999 4:30:30","last_active":"task789","tags":[4]}
-    fake4 = {"version":"xxx-xxx-000","status":4,"creation":"04 Dec 1995 00:12:00","last_mod":"04 Dec 1995 00:12:00","last_active":"task000","tags":[1,2]}
-    fakeData = [fake1, fake2, fake3, fake4]
-
-    return jsonify(fakeData)
+    # fake1 = {"version":"xxx-xxx-123","status":1,"creation":"01 Jan 1970 00:00:00","last_mod":"01 Jan 1970 00:00:00","last_active":"task123","tags":[1,3]}
+    # fake2 = {"version":"xxx-xxx-456","status":2,"creation":"04 Dec 2017 00:12:00","last_mod":"04 Dec 2017 00:12:00","last_active":"task456","tags":[2]}
+    # fake3 = {"version":"xxx-xxx-789","status":3,"creation":"29 Jun 1999 4:30:30","last_mod":"29 Jun 1999 4:30:30","last_active":"task789","tags":[4]}
+    # fake4 = {"version":"xxx-xxx-000","status":4,"creation":"04 Dec 1995 00:12:00","last_mod":"04 Dec 1995 00:12:00","last_active":"task000","tags":[1,2]}
+    # fakeData = [fake1, fake2, fake3, fake4]
+    fakeData = memcache.get('releases')
+    if request.method = 'GET':
+        return jsonify(fakeData), 200
