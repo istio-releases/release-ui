3c5b19cc7f80b9cb253924cf73e1e9fe5b2371e9
DomMarq <dommarques@google.com>
2018-06-08 16:33:26 -0700

    Added memcache read from fake_data.json

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from flask import Flask, jsonify, request
-<<<<<<< HEAD
 from flask_restful import Api, Resource, reqparse
 import read_fake_data
 
 
-=======
-from flask_restful import Resource, Api
->>>>>>> 1ffcb6be6fc470ab3dfd397b1cccae91d14dd2bf
 
 # creating the Flask application
 app = Flask(__name__)
 api = Api(app)
-<<<<<<< HEAD
-read_fake_data # read and load the data from fake_data.py into memcache
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # fake1 = {"version":"xxx-xxx-123","status":1,"creation":"01 Jan 1970 00:00:00","last_mod":"01 Jan 1970 00:00:00","last_active":"task123","tags":[1,3]}
-    # fake2 = {"version":"xxx-xxx-456","status":2,"creation":"04 Dec 2017 00:12:00","last_mod":"04 Dec 2017 00:12:00","last_active":"task456","tags":[2]}
-    # fake3 = {"version":"xxx-xxx-789","status":3,"creation":"29 Jun 1999 4:30:30","last_mod":"29 Jun 1999 4:30:30","last_active":"task789","tags":[4]}
-    # fake4 = {"version":"xxx-xxx-000","status":4,"creation":"04 Dec 1995 00:12:00","last_mod":"04 Dec 1995 00:12:00","last_active":"task000","tags":[1,2]}
-    # fakeData = [fake1, fake2, fake3, fake4]
-    fakeData = memcache.get('releases')
-    if request.method = 'GET':
-        return jsonify(fakeData), 200
-=======
-
+read_fake_data # reads fake_data.json and puts it in memecache
 
 @app.route('/')
 def index():
@@ -36,4 +19,3 @@ def release_list():
     # Get list of releases from memcache or adapter interface
     result = ""
     return Response(json.dumps(result), mimetype="application/json")
->>>>>>> 1ffcb6be6fc470ab3dfd397b1cccae91d14dd2bf
