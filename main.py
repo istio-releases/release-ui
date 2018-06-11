6be4e53fde6626efcfdc8a367a4ac47b527bf551
cabreraem <emiliacabrera@emiliacabrera.svl.corp.google.com>
2018-06-11 15:58:46 -0700

    main.py changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 import read_fake_data
 from google.appenginge.api import memcache
 
 
 # creating the Flask application
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 read_fake_data # reads fake_data.json and puts it in memecache
 
