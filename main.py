7389882c6bcad3220cfbbdf72a19201a5e31f712
cabreraem <emiliacabrera@emiliacabrera.svl.corp.google.com>
2018-06-12 11:12:06 -0700

    fixed bugs

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_restful import Api, Resource, reqparse
 from flask import send_file, make_response, abort
 from flask.views import MethodView
 import json
-import read_fake_data
 from google.appengine.api import memcache
 
 
