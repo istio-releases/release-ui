37f601a20a81b183255a00051e85dad20d78b2c2
DomMarq <dommarques@google.com>
2018-06-13 16:07:07 -0700

    pagination now returns in JSON format

--- main.py
+++ main.py
@@ -128,13 +128,13 @@ class Pagination(Resource):
 
         memcache_exists, memcache_results = in_memcache(args)
         if memcache_exists:
-            return memcache_results[int(args['offset']):int(args['limit'])]
+            return json.dumps(memcache_results[int(args['offset']):int(args['limit'])])
         else:
             response = filter(args['state'], args['label'], args['start_date'], args['end_date'], args['datetype'])
             response = sort(response, args['sort_method'])
             memcache.add(key=memcache_results, value=response, time=3600)
             # time adds an expiration time of one hour, in order to keep the memcache somewhat up to date
-            return response[int(args['offset']):int(args['limit'])]
+            return json.dumps(response[int(args['offset']):int(args['limit'])])
 
 
 
