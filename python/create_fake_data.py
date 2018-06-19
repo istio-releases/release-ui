41b2fc94a48b43be797cc889df60c86b59c1d1dc
cabreraem <emiliacabrera@emiliacabrera.svl.corp.google.com>
2018-06-19 11:52:25 -0700

    task dictionary

--- python/create_fake_data.py
+++ python/create_fake_data.py
@@ -30,23 +30,31 @@ for i in range(number_of_releases):
     new_data[release_id]['last_modified'] = random.randint(0, 1528996059)
     new_data[release_id]['artifacts_link'] = 'https://youtu.be/dQw4w9WgXcQ'
     new_data[release_id]['release_url'] = 'https://youtu.be/dQw4w9WgXcQ'
-    new_data[release_id]['tasks'] = {}
+    new_data[release_id]['tasks'] = []
     for task_num in range(number_of_tasks):
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID'] = {}
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['task_name'] = 'task-' + str(task_num)
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['started'] = random.randint(0, 2147483647)
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['last_modified'] = random.randint(0, 2147483647)
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['status'] = random.randint(1,4)
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['error'] = 'error number ' + str(random.randint(0,1023))
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['log_url'] = 'https://youtu.be/dQw4w9WgXcQ'
-        new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['dependent_on'] = []
-        for prev_task in range(0,task_num):
-            new_data[release_id]['tasks']['task'+str(task_num)+'_ID']['dependent_on'].append('task' + str(prev_task) + '_ID')
+        new_data[release_id]['tasks'].append(random.randint(0,9))
     new_data[release_id]['last_active_task'] = 'task' + str(random.randint(1,5)) + '_ID'
     print  release_id + ' created'
 
 #---Puts the JSON into a file called fake_data.json---#
-fake_data = open("fake_data.json", "w+")
+fake_data = open("fake_release_data.json", "w+")
 json.dump(new_data, fake_data, indent=2 )
 
-print 'Created a file called fake_data.json'
+print 'Created a file called fake_release_data.json'
+
+new_data = {}
+for i in range(10):
+    task_id = "task-" + str(i)
+    new_data[task_id] = {}
+    new_data[task_id]['task_name'] = task_id
+    new_data[task_id]['started'] = random.randint(0, 2147483647)
+    new_data[task_id]['last_modified'] = random.randint(0, 2147483647)
+    new_data[task_id]['status'] = random.randint(1,4)
+    new_data[task_id]['error'] = 'error number ' + str(random.randint(0,1023))
+    new_data[task_id]['log_url'] = 'https://youtu.be/dQw4w9WgXcQ'
+    new_data[task_id]['dependent_on'] = []
+    print  task_id + ' created'
+
+#---Puts the JSON into a file called fake_data.json---#
+fake_data = open("fake_task_data.json", "w+")
+json.dump(new_data, fake_data, indent=2 )
