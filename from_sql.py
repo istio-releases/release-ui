ab571b58d5b7254ce42244481f9721f240fb4955
DomMarq <dommarques@google.com>
2018-07-13 17:26:24 -0700

    Kind of fixed no tasks bug(more of a band-aid)

--- from_sql.py
+++ from_sql.py
@@ -32,6 +32,7 @@ def read_releases(release_data, airflow_db):
       release.last_active_task = most_recent_task.task_name
     else:
       release.last_modified = to_timestamp(item[2])
+      release.last_active_task = ''
     release_objects[release.name] = release
 
   return release_objects
@@ -49,6 +50,7 @@ def read_tasks(task_data):
   task_objects = []
   for item in task_data:
     task = Task()
+
     task.task_name = item[0]
     task.add_dependency = None  # TODO(dommarques): figure out how to get this - dag info?
     task.started = to_timestamp(item[3])
@@ -57,6 +59,8 @@ def read_tasks(task_data):
     task.log_url = "https://youtu.be/dQw4w9WgXcQ"  # TODO(dommarques): figure out how to get the log in here
     task.error = item[6]
     task_objects.append(task)
+    print '********** OBJECTS IN READ'
+    print task_objects
   return task_objects
 
 
@@ -73,11 +77,14 @@ def get_task_info(execution_date, airflow_db):
   """
   task_query = to_sql_tasks(execution_date)
   task_data = airflow_db.query(task_query)
+  print task_data
   task_objects = read_tasks(task_data)
   state = 0
   task_ids = []
+  print '***** OBJECTS IN GET_INFO'
+  print task_objects
   if task_objects:
-    most_recent_task = task_objects[-1]
+    most_recent_task = task_objects[len(task_objects) - 1]
   else:
     most_recent_task = None
   for task in task_objects:
