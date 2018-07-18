d742cdc1ce3efe1623f1b122ee97e77963b49896
Emilia Cabrera <emiliakcabrera@gmail.com>
2018-07-18 16:05:10 -0700

    fixed comments

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,4 +1,3 @@
 import sys
-sys.path.append("../")
 
 __all__ = ["filter_releases", "resources"]
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
 import sys
-sys.path.append("../")
 
 __all__ = ["file_adapter_tests", "filter_tests", "release_tests", "sort_test", "task_tests"]
