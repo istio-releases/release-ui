8dfc1f967b0fce49fb84907f0822dc14b63dd5b4
Emilia Cabrera <emiliakcabrera@gmail.com>
2018-07-26 16:49:50 -0700

    another bug

--- data/filter_options.py
+++ data/filter_options.py
@@ -32,7 +32,7 @@ class FilterOptions(object):
       raise ValueError(error)
 
   def _is_valid_state(self, state):
-    return 0 <= state <= 4
+    return 0 <= state <= 5
 
   @property
   def start_date(self):
